cf21de222246a7aebafd3b5340118a8c73d80998
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-08-08 16:15:40 -0400

    add component folders with files for navbar and projects, fill out nav file, start editing main and app files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, useLocation } from 'react-router-dom';
+
+function Navigation() {
+    const currentPage = useLocation().pathname;
+
+    return (
+        <Navbar expand='lg' className='bg-body-tertiary'>
+            <Container>
+                <Navbar.Brand className='full-name'>Elvis Lau</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='ms-auto'>
+                        <Link
+                            to="/"
+                            className={currentPage === "/" ? "nav-link active" : "nav-link"}
+                        >
+                            About Me
+                        </Link>
+                        <Link 
+                            to="/portfolio"
+                            className={currentPage === "/portfolio" ? "nav-link active": "nav-link"}
+                        >
+                            Portfolio
+                        </Link>
+                        <Link 
+                            to="/contact"
+                            className={currentPage === "/contact" ? "nav-link active": "nav-link"}
+                        >
+                            Contact
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+
+        </Navbar>
+    );
+}
+
+export default Navigation;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: '/About',
+    //     element: <About />,
+    //   },
+    //   {
+    //     path: '/Blog',
+    //     element: <Blog />,
+    //   },
+    //   {
+    //     path: '/Contact',
+    //     element: <Contact />,
+    //   },
+    // ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
