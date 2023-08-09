988964675b0a78df571b07e67be478e5c35b32e2
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-08-08 23:37:18 -0400

    add screenshots of projects in images folder in assets and import it into project component file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,34 +1,40 @@
+import EndWorldThirst from '../assets/images/end-world-thirst-website.png'
+import LordOfTheFlies from '../assets/images/lord-of-the-pies-website.png'
+import DulyNoted from '../assets/images/duly-noted-text-editor.png'
+import WeatherDashboard from '../assets/images/weather-dashboard-livesite.png'
+import WorkDayScheduler from '../assets/images/work-day-scheduler-page.png'
+
 export default function Projects() {
     const projs = [
         {
             name: "End World Thirst",
             link: "https://myrojoylee.github.io/end-world-thirst/",
             languages: "HTML/CSS/JS",
-            siteImage: ""
+            siteImage: EndWorldThirst
         },
         {
             name: "Lord of the Pies",
             link: "https://lord-of-the-pies-f3c957a9b4a8.herokuapp.com/",
             languages: "Handlebars/CSS/JS",
-            siteImage: ""
+            siteImage: LordOfTheFlies
         },
         {
             name: "Duly Noted",
             link: "https://duly-noted-text-editor-5ac1387f2bf2.herokuapp.com/",
             languages: "HTML/CSS/JS",
-            siteImage: ""
+            siteImage: DulyNoted
         },
         {
             name: "Weather Dashboard",
             link: "https://elvislau74.github.io/weather-dashboard/",
             languages: "HTML/CSS/JS",
-            siteImage: ""
+            siteImage: WeatherDashboard
         },
         {
             name: "Work Day Scheduler",
             link: "https://elvislau74.github.io/work-day-scheduler/",
             languages: "HTML/CSS/JS",
-            siteImage: ""
+            siteImage: WorkDayScheduler
         },
     ]
 
