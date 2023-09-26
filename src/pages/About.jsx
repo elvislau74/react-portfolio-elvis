de0440b91b939f322a02de7e6d6b12cf5e9df819
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-09-26 13:20:32 -0400

    update aboutme page description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,16 +13,19 @@ export default function About() {
             
             <section className="text-description">
                 <p>
-                    Hi, my name is Elvis Lau. I am currently learning to become a Front-End Developer 
-                    at the UPenn LPS Bootcamp. In my free time, I like to play videogames, listen to 
-                    music and watch movies. 
+                    Hi, my name is Elvis Lau. I am a Full Stack Web Developer who recently graduated from the 
+                    University of Pennsylvania's LPS Coding Bootcamp. In my free time, I enjoy playing videogames, listening to 
+                    music and watching movies. 
                 </p>
                 <p>
-                    My first experience with coding was in College, where I took a few Computer Science courses. 
-                    Although the debugging process can be really troublesome, I find myself really enjoying it.
+                    My first experience with coding was in College, where I took a few Computer Science courses and
+                    learned the basics of Javascript. I enjoy the problem solving aspect of coding and the satisfaction 
+                    of seeing my code work and ideas coming to fruition. Going through the bootcamp has helped to solidify my 
+                    knowledge of coding and further develop my troubleshooting skills. 
                 </p>
                 <p>
-                    I hope to use everything I have learned to improve and transform any projects you may have. 
+                    I look forward to utilizing everything that I have learned to improve and transform any projects 
+                    or ideas you may have. 
                 </p>
             </section>
         </div>
